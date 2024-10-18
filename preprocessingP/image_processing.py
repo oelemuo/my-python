9a89a7ca516af3919edc28a92f8c6dbf63411ed0
Obinna <so01811@umbc.edu>
2024-10-18 16:31:53 -0400

    added new files

--- preprocessingP/image_processing.py
+++ preprocessingP/image_processing.py
@@ -1,2 +1,15 @@
-import numpy as np # workingn with arrays
-import cv2
+import numpy as np # importy for workingn with arrays
+import cv2 # import for working with images
+
+# Function which Loads the images using OpenCV
+def adjust_brightness(image_path, brightness_value=50):
+    image = cv2.imread(image_path) #reads image as a NumPy array
+    #This adds brightness to each pixel in the image
+    brightened_image = np.clip(image + brightness_value, 0, 255)
+
+    #save the new brightened image
+    cv2.imwrite(r'C:\Users\obinn\Desktop\brightened_image.jpg', brightened_image)
+    return brightened_image
+
+#example usage
+adjust_brightness(r'C:\Users\obinn\Downloads\893dce4ad3ddfdf0c8487c652216590a (1).jpg',50)
